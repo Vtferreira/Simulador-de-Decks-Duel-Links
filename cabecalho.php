276096426654aa943b2a7dd31eeaea6bd3b0a2a3
Vinicius Tarcisio <tfvinicius@gmail.com>
2018-05-20 14:00:08 -0300

    comentários das alterações

---
+++ cabecalho.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Simulador de Decks para Duel Links !</title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/loja.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <div class="container">
+      <div class="principal">
